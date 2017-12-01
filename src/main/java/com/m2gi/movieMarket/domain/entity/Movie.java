525575c8ee79af4fb1f54f8e6d113eaba20295d3
Anthony <chavoutier.anthony@gmail.com>
2017-12-01 21:40:44 +0100

    Fix fetch type

--- src/main/java/com/m2gi/movieMarket/domain/entity/Movie.java
+++ src/main/java/com/m2gi/movieMarket/domain/entity/Movie.java
@@ -13,7 +13,7 @@ public class Movie implements Serializable {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	
-	@ManyToMany(cascade = { CascadeType.ALL })
+	@ManyToMany(fetch = FetchType.EAGER, cascade = { CascadeType.ALL })
 	@JoinTable(
 		name = "movie_category",
 		joinColumns = { @JoinColumn(name = "movie_id") },
