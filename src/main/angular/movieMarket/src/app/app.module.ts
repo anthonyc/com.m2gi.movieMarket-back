c2abcb39bd5a7646de1fb3c2e274327624a5c08e
Anthony <chavoutier.anthony@gmail.com>
2017-12-19 00:04:39 +0100

    Basic breadcrumb

--- src/main/angular/movieMarket/src/app/app.module.ts
+++ src/main/angular/movieMarket/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {ModuleWithProviders, NgModule} from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -24,20 +24,47 @@ import { HttpClientModule } from "@angular/common/http";
 import { StoreButtonComponentComponent } from './component/store-button-component/store-button-component.component';
 import { CartComponent } from './component/cart/cart.component';
 import { CartDetailComponent } from './component/cart-detail/cart-detail.component';
-import 'hammerjs';
 import { LoginComponent } from './component/login/login.component';
 import {AuthenticateService} from "./service/authenticate.service";
 import { UserAccountComponent } from './component/user-account/user-account.component';
+import 'hammerjs';
 
 
-export const appRoutes: Routes = [
-  { path: '', component: HomePageContentComponent },
-  { path: 'movies', component: SearchResultContentComponent },
-  { path: 'movies/:id', component: MovieDetailComponent },
-  { path: 'category/movie/:category', component: MovieListComponent },
-  { path: 'user/create', component: CreateUserComponent },
-  { path: 'cart', component: CartComponent},
-  { path: 'account/:id', component: UserAccountComponent }
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageContentComponent,
+  },
+  {
+    path: 'category/movie/:category',
+    component: MovieListComponent,
+    data: {
+      breadcrumb: "Liste de films"
+    },
+  },
+  {
+    path: 'movies/:id',
+    component: MovieDetailComponent,
+    data: {
+      breadcrumb: "Détail"
+    }
+  },
+  { path: 'cart', component: CartComponent, data: {
+      breadcrumb: "Panier"
+    }
+  },
+  { path: 'movies', component: SearchResultContentComponent, data: {
+      breadcrumb: "Recherche"
+    }
+  },
+  { path: 'user/create', component: CreateUserComponent, data: {
+      breadcrumb: "Création utilisateur"
+    }
+  },
+  { path: 'account/:id', component: UserAccountComponent, data: {
+      breadcrumb: "Compte"
+    }
+  }
 ];
 
 @NgModule({
@@ -63,7 +90,7 @@ export const appRoutes: Routes = [
     BrowserModule,
     HttpModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(routes),
     FormsModule,
     ReactiveFormsModule,
     NgxCarouselModule
