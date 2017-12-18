39bc073f66a86bc4b8414cdbc35a939385e6015a
Sylvain MARION <s-marion@hotmail.fr>
2017-12-18 17:37:53 +0100

    More changes to the cart page. Validate button, total price (design to define). Spinner loader on movie pages to fix the error for asynchrone loading.

--- src/main/angular/movieMarket/src/app/component/cart-detail/cart-detail.component.ts
+++ src/main/angular/movieMarket/src/app/component/cart-detail/cart-detail.component.ts
@@ -1,21 +1,34 @@
 import { CartComponent } from './../cart/cart.component';
 import { CartService } from './../../service/cart.service';
 import { CartDetail } from './../../model/cart-detail';
-import {Component, Input, OnChanges, Output, EventEmitter} from '@angular/core';
+import {Component, Input, OnChanges, Output, EventEmitter, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-cart-detail',
   templateUrl: './cart-detail.component.html',
   styleUrls: ['./cart-detail.component.css']
 })
-export class CartDetailComponent implements OnChanges {
+export class CartDetailComponent implements OnChanges, OnInit {
 
   @Input()
   cartDetail: CartDetail;
 
+  shortDesc: string;
+
   constructor(private cartService: CartService) {}
 
   ngOnChanges() { }
+
+  ngOnInit() {
+    this.shortDesc = this.cartDetail.movie.description.substring(0, 300);
+    if (this.shortDesc.length < this.cartDetail.movie.description.length) {
+      let i: number;
+      for (i = this.shortDesc.length; this.shortDesc[i] !== ' '; i--) {}
+      this.shortDesc = this.shortDesc.substring(0, i);
+      this.shortDesc += '...';
+    }
+
+  }
 
   addMovie() {
     this.cartDetail = this.cartService.addMovie(this.cartDetail.movie);
